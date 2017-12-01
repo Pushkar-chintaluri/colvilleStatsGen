ed468f199deadb8c0cf8657ad2bc95e400b332f3
cvpushkar <Pushkar-chintaluri@users.noreply.github.com>
2017-12-01 13:03:44 -0600

    Update statsGen.py

--- statsGen.py
+++ statsGen.py
@@ -1,10 +1,10 @@
 import random
 
 def getStats():
-    return [sum(i) for i in [sorted([random.randint(1,6) for a in range(1,5)])[1:] for each in range(1,7)]]
+    return [sum(stat) for stat in [sorted([random.randint(1,6) for roll in range(1,5)])[1:] for att in range(1,7)]]
 
 def printStats(stats):
-    if len([s for s in stats if s>=15])>=2:
+    if len([stat for stat in stats if stat>=15])>=2:
         print (stats)
     else:
         printStats(getStats())
